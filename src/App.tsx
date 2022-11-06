d78de0f049cbc18c56822364a7374ed4d519f31d
suzuki-kenta0 <89818757+suzuki-kenta0@users.noreply.github.com>
2022-11-06 20:51:39 +0900

    chore:eslint+prettierの導入

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,26 @@
-import React from 'react'
+import React from 'react';
 import {
-    BrowserRouter,
-    Routes,
-    Route,
-} from 'react-router-dom'
-import Home from './pages/Home'
-import Posts from './pages/Posts'
-import Layout from './components/Layout'
-import Post from './components/Post'
-import Calendar from './components/Calendar'
- 
-const App: React.FC = () => {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Layout />}>
-                    <Route index element={<Home />} />
-                    <Route path="posts" element={<Posts />} />
-                    <Route path="calendar" element={<Calendar />} />
-                    <Route path="posts/:postId" element={<Post />} />
-                </Route>
-            </Routes>
-        </BrowserRouter>
-    )
-}
- 
+	BrowserRouter,
+	Routes,
+	Route,
+} from 'react-router-dom';
+import Home from './pages/Home';
+import Posts from './pages/Posts';
+import Layout from './components/Layout';
+import Post from './components/Post';
+import Calendar from './components/Calendar';
 
-export default App
+const App: React.FC = () => (
+	<BrowserRouter>
+		<Routes>
+			<Route path='/' element={<Layout />}>
+				<Route index element={<Home />} />
+				<Route path='posts' element={<Posts />} />
+				<Route path='calendar' element={<Calendar />} />
+				<Route path='posts/:postId' element={<Post />} />
+			</Route>
+		</Routes>
+	</BrowserRouter>
+);
+
+export default App;
--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
- 
+import React from 'react';
+import {useParams} from 'react-router-dom';
+
 const Post: React.FC = () => {
-    const { postId } = useParams()
-     
-    return (
-        <div>
-            <h2>投稿詳細</h2>
-            <p>ID: { postId }</p>
-        </div>
-    )
-}
- 
-export default Post
+	const {postId} = useParams();
+
+	return (
+		<div>
+			<h2>投稿詳細</h2>
+			<p>ID: { postId }</p>
+		</div>
+	);
+};
+
+export default Post;
