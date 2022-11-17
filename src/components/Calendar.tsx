14de2575bfcfa4a4ed87481519a5e71e171420bb
suzuki-kenta0 <suzuki@key-p.co.jp>
2022-11-17 19:31:19 +0900

    feat:一時保存

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,32 +1,43 @@
-import {Calendar, momentLocalizer} from 'react-big-calendar';
-import moment from 'moment';
+import { Calendar, momentLocalizer } from 'react-big-calendar'
+import moment from 'moment'
 
-import 'react-big-calendar/lib/addons/dragAndDrop/styles.css';
-import 'react-big-calendar/lib/css/react-big-calendar.css';
+import 'react-big-calendar/lib/addons/dragAndDrop/styles.css'
+import 'react-big-calendar/lib/css/react-big-calendar.css'
 
-const localizer = momentLocalizer(moment);
+const localizer = momentLocalizer(moment)
 const myEventsList = [
-	{
-		id: 0,
-		title: 'My event',
-		start: new Date(),
-		end: new Date(),
-	},
-];
+  {
+    id: 0,
+    title: 'aaa',
+    start: moment({ hours: 8 }).toDate(),
+    end: moment({ hours: 9 }).toDate()
+  }
+]
+
+const handleSelectSlot = () => {
+  console.log(22222)
+  myEventsList.push({
+    id: 1,
+    title: 'test',
+    start: moment({ hours: 10 }).toDate(),
+    end: moment({ hours: 11 }).toDate()
+  })
+}
 
 const MyCalendar = () => (
-	<div>
-		<Calendar
-			localizer={localizer}
-			events={myEventsList}
-			startAccessor='start'
-			endAccessor='end'
-			defaultView='day'
-			toolbar={false}
-			style={{height: 500, width: 200}}
-			selectable={true}
-		/>
-	</div>
-);
+  <div>
+    <Calendar
+      localizer={localizer}
+      events={myEventsList}
+      startAccessor="start"
+      endAccessor="end"
+      defaultView="day"
+      onSelectSlot={handleSelectSlot}
+      selectable
+      toolbar={false}
+      style={{ height: 500, width: 200 }}
+    />
+  </div>
+)
 
-export default MyCalendar;
+export default MyCalendar
