b796c0f0aa6c58609ae06f0e59ee7dd0717ecf74
suzuken0424 <s.kenta0424@gmail.com>
2022-12-03 10:00:46 +0900

    一時保存

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,43 +1,62 @@
-import { Calendar, momentLocalizer } from 'react-big-calendar'
-import moment from 'moment'
+import {Calendar, momentLocalizer} from 'react-big-calendar';
+import moment from 'moment';
+import React, {useState} from 'react';
+import {compareAsc, format, parse} from 'date-fns';
 
-import 'react-big-calendar/lib/addons/dragAndDrop/styles.css'
-import 'react-big-calendar/lib/css/react-big-calendar.css'
+import 'react-big-calendar/lib/addons/dragAndDrop/styles.css';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
 
-const localizer = momentLocalizer(moment)
-const myEventsList = [
-  {
-    id: 0,
-    title: 'aaa',
-    start: moment({ hours: 8 }).toDate(),
-    end: moment({ hours: 9 }).toDate()
-  }
-]
+const localizer = momentLocalizer(moment);
 
-const handleSelectSlot = () => {
-  console.log(22222)
-  myEventsList.push({
-    id: 1,
-    title: 'test',
-    start: moment({ hours: 10 }).toDate(),
-    end: moment({ hours: 11 }).toDate()
-  })
-}
+type Events = {
+	title: string;
+	start: Date;
+	end: Date;
+};
 
-const MyCalendar = () => (
-  <div>
-    <Calendar
-      localizer={localizer}
-      events={myEventsList}
-      startAccessor="start"
-      endAccessor="end"
-      defaultView="day"
-      onSelectSlot={handleSelectSlot}
-      selectable
-      toolbar={false}
-      style={{ height: 500, width: 200 }}
-    />
-  </div>
-)
+const MyCalendar = () => {
+	const [events, setEvents] = useState<Events[]>([
+		{
+			title: 'event1',
+			start: parse('2022-11-18 09:00:00', 'yyyy-MM-dd HH:mm:ss', new Date()),
+			end: parse('2022-11-18 10:00:00', 'yyyy-MM-dd HH:mm:ss', new Date()),
+		},
+		// {
+		// 	id: 2,
+		// 	title: 'event2',
+		// 	start: moment({hours: 10}).toDate(),
+		// 	end: moment({hours: 11}).toDate(),
+		// },
+		// {
+		// 	id: 3,
+		// 	title: 'event3',
+		// 	start: moment({hours: 11}).toDate(),
+		// 	end: moment({hours: 12}).toDate(),
+		// },
+	]);
+	const newEvent = () => {
+		setEvents([...events, {
+			title: 'Hello React',
+			start: new Date(2022, 11, 18, 8),
+			end: new Date(2022, 11, 18, 11),
+		}]);
+	};
 
-export default MyCalendar
+	return (
+		<div>
+			<Calendar
+				localizer={localizer}
+				events={events}
+				startAccessor='start'
+				endAccessor='end'
+				defaultView='day'
+				onSelectSlot={newEvent}
+				selectable
+				toolbar={false}
+				style={{height: 500, width: 200}}
+			/>
+		</div>
+	);
+};
+
+export default MyCalendar;
